ec9980abe1440f7d1e87b86a0c1c9efdb05f678b
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-04-11 07:44:57 +0700

    Add UI, Models sample

---
+++ app/src/main/java/com/coffee/app/model/Product.java
@@ -0,0 +1,4 @@
+package com.coffee.app.model;
+
+public class Product {
+}
---
+++ app/src/main/java/com/coffee/app/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.coffee.app.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coffee.app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
